477647893b0620947f15267609fd0ef3802faac8
Pham The Anh <anh.pham@dek-cz.com>
2022-01-13 16:49:23 +0100

    autoloader for php files within src folder

--- public/index.php
+++ public/index.php
@@ -23,4 +23,22 @@ $app->addMiddleware(
 
 require __DIR__ . '/../src/routes.php';
 
+function load_php_files($directory) {
+    if(is_dir($directory)) {
+        $scan = scandir($directory);
+        unset($scan[0], $scan[1]); //unset . and ..
+        foreach($scan as $file) {
+            if(is_dir($directory."/".$file)) {
+                load_php_files($directory."/".$file);
+            } else {
+                if(strpos($file, '.php') !== false) {
+                    include_once($directory."/".$file);
+                }
+            }
+        }
+    }
+}
+
+load_php_files(__DIR__ . '/../src');
+
 $app->run();
